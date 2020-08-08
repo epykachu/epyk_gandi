4b869437e97f67aeb39c908149334ae911e51844
Tinelsi972 <epykachu@gmail.com>
2020-08-08 14:54:23 +0100

    simple flask app

--- epyk_server/app.py
+++ epyk_server/app.py
@@ -1,4 +1,9 @@
 from flask import Flask
-
-
+from epyk_server.endpoints import welcome
 app = Flask()
+
+
+
+def init_app():
+    global app
+    app.register_blueprint(welcome)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from epyk_server.app import app, init_app
+
+init_app()
+application = app
