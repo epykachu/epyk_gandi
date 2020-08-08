2badd9253cdf21ef0cad00a01b32718c5915c32f
Tinelsi972 <epykachu@gmail.com>
2020-08-08 14:58:27 +0100

    simple flask app

--- epyk_server/app.py
+++ epyk_server/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from epyk_server.endpoints import welcome
-app = Flask()
 
 
+app = Flask(__name__)
 
 def init_app():
     global app
-    app.register_blueprint(welcome)
+    app.register_blueprint(welcome.welcome)
+
+
+if __name__ == '__main__':
+    init_app()
+    app.run()
